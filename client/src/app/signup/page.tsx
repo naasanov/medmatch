60529eca04be6c1fd96bd6f0f025e110624c0029
Aidan Scott <160932113+AidanJScott@users.noreply.github.com>
2025-04-16 15:07:19 -0400

    added basic react hook form elements to signup

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -1,7 +1,133 @@
 "use client";
 import { useRef, useState, useEffect } from 'react';
 import Link from 'next/link';
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
+// zod defines form shape and fields
+const formSchema = z.object({
+  first: z.string().min(1).max(50),
+  last: z.string().min(1).max(50),
+  phone: z.string(), // figure out phone number validation - might be a different library than zod
+  birthday: z.string().date(),
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
+export default function SignUpForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      first: "",
+      last: "",
+      phone: "",
+      birthday: "",
+      email: "",
+      password: "",
+    },
+    
+  })
+ 
+  // submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="first"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="first name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="last"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="last name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="birthday"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="birthday mm/dd/yyyy" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="phone number (xxx) xxx-xxxx" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password "placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Register</Button>
+      </form>
+    </Form>
+  )
+}
+
+/*
 function Signup() {
   const firstRef = useRef<HTMLInputElement>(null);
 
@@ -62,3 +188,4 @@ function Signup() {
 }
 
 export default Signup;
+*/
